7d226b9658b75ed1d8157ef900e52b114ec6610b
azuelsdorf16 <azuelsdorf16@gmail.com>
2021-02-20 23:48:54 -0500

    Fixing bug where URL is mismatched.

--- remember_to_smile.js
+++ remember_to_smile.js
@@ -3,7 +3,7 @@
 const amazonDomains = ["amazon.co.uk", "amazon.com", "amazon.de"];
 
 for (let i = 0; i < amazonDomains.length; ++i) {
-    if (window.location.href == "https://www." + amazonDomains[i]) {
+    if (window.location.href == "https://www." + amazonDomains[i] + "/") {
         const message = "Hello! It looks like you're visiting " + amazonDomains[i] + ". Would you consider visiting smile." + amazonDomains[i] + " instead? For no extra cost to you, a portion of the price you pay for eligible products on smile." + amazonDomains[i] + " will be donated to a charitable organization of your choice. Click OK to visit smile." + amazonDomains[i] + ". Click Cancel to decline.";
 
         if (window.confirm(message)) {
