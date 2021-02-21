6c35d044d2b74fe8a6f33c7c5bb099a1deae492e
azuelsdorf16 <azuelsdorf16@gmail.com>
2021-02-20 22:58:56 -0500

    Fixing message for sites other than amazon.com

--- remember_to_smile.js
+++ remember_to_smile.js
@@ -1,11 +1,11 @@
 // Copyright (c) 2021 Andrew Zuelsdorf. All rights reserved.
 
-const message = "Hello! It looks like you're visiting www.amazon.com. Would you consider visiting smile.amazon.com instead? For no extra cost to you, a portion of the price you pay for eligible products on smile.amazon.com will be donated to a charitable organization of your choice. Click OK to visit smile.amazon.com. Click Cancel to decline.";
-
 const amazonDomains = ["amazon.co.uk", "amazon.com", "amazon.de"];
 
 for (let i = 0; i < amazonDomains.length; ++i) {
     if (window.location.href == "https://www." + amazonDomains[i]) {
+        const message = "Hello! It looks like you're visiting " + amazonDomains[i] + ". Would you consider visiting smile." + amazonDomains[i] + " instead? For no extra cost to you, a portion of the price you pay for eligible products on smile." + amazonDomains[i] + " will be donated to a charitable organization of your choice. Click OK to visit smile." + amazonDomains[i] + ". Click Cancel to decline.";
+
         if (window.confirm(message)) {
             window.location.href = "https://smile." + amazonDomains[i];
         }
